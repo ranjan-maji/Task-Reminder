29bd48287488057cdd58b328e0333bfd0e8bd483
ranjan-maji <ranjanmaji906@gmail.com>
2023-04-04 15:33:52 +0530

    first4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom'
+import { FaFacebook ,FaTwitter} from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright &copy; 2023</p>
+      <p>By Ranjan &copy; 2023</p>
       <Link to='/about'>About</Link>
+      <br />
+        <span>
+          <FaFacebook />
+        </span>
+        <span>
+        <FaTwitter />
+        </span>
     </footer>
   )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaTimes} from 'react-icons/fa'
+import { FaTimes , FaFacebook} from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle  }) => {
   return (
